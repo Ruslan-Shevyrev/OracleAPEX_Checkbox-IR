c833b0942633f3100ee43a588d9e543213ac3910
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2022-11-01 15:00:18 +0300

    add DELETE_ELEMENT_COLLECTION.sql

---
+++ DELETE_ELEMENT_COLLECTION.sql
@@ -0,0 +1,16 @@
+BEGIN
+	FOR c IN (DELECT SEQ_ID
+				FROM apex_collections
+				WHERE collection_name = apex_application.g_x02
+				AND n001 = apex_application.g_x01)
+    LOOP
+        APEX_COLLECTION.DELETE_MEMBER (p_collection_name => apex_application.g_x02,
+									   p_seq             => c.SEQ_ID);
+    END LOOP;
+    
+	htp.p('200');
+EXCEPTION WHEN OTHERS THEN  
+	htp.p(SQLERRM);
+    ROLLBACK;
+    htp.p('500');  
+end;
