a206ebd5b855528675a8e5b8c5d8fdf244a38e6d
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2022-11-02 09:31:58 +0300

    add ADD_ELEMENT_COLLECTION.sql

--- ADD_ELEMENT_COLLECTION.sql
+++ ADD_ELEMENT_COLLECTION.sql
@@ -2,7 +2,16 @@ BEGIN
     IF apex_collection.collection_exists(apex_application.g_x02) <> TRUE THEN
 		apex_collection.create_collection(p_collection_name => apex_application.g_x02);
     END IF;
-
+	
+	FOR c IN (DELECT SEQ_ID
+				FROM apex_collections
+				WHERE collection_name = apex_application.g_x02
+				AND n001 = apex_application.g_x01)
+    LOOP
+        APEX_COLLECTION.DELETE_MEMBER (p_collection_name => apex_application.g_x02,
+									   p_seq             => c.SEQ_ID);
+    END LOOP;
+	
     APEX_COLLECTION.ADD_MEMBER(
         p_collection_name => apex_application.g_x02,
         p_n001            => apex_application.g_x01);
