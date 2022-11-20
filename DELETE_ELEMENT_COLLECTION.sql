bb9f2fde668b7fda8003227f1a290a6dc8295baf
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2022-11-20 10:43:12 +0300

    add DELETE_ELEMENT_COLLECTION.sql

--- DELETE_ELEMENT_COLLECTION.sql
+++ DELETE_ELEMENT_COLLECTION.sql
@@ -4,9 +4,9 @@ BEGIN
 				WHERE collection_name = apex_application.g_x02
 				AND n001 = apex_application.g_x01)
 	LOOP
-		APEX_COLLECTION.DELETE_MEMBER (p_collection_name => apex_application.g_x02,
-										p_seq             => c.SEQ_ID);
-    END LOOP;
+		APEX_COLLECTION.DELETE_MEMBER (p_collection_name	=> apex_application.g_x02,
+										p_seq				=> c.SEQ_ID);
+	END LOOP;
 
 	htp.p('200');
 EXCEPTION WHEN OTHERS THEN
